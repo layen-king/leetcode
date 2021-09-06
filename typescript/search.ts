fb87f3aacea6572403be2e0767463a14c0d8f0ff
layen <446495695@qq.com>
2021-09-06 09:47:51 +0800

    二分查找与全部遍历

---
+++ typescript/search.ts
@@ -0,0 +1,60 @@
+// 二分查找
+// 简单
+
+// 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+
+// 示例 1:
+
+// 输入: nums = [-1,0,3,5,9,12], target = 9
+// 输出: 4
+// 解释: 9 出现在 nums 中并且下标为 4
+// 示例 2:
+
+// 输入: nums = [-1,0,3,5,9,12], target = 2
+// 输出: -1
+// 解释: 2 不存在 nums 中因此返回 -1
+//  
+
+// 提示：
+
+// 你可以假设 nums 中的所有元素是不重复的。
+// n 将在 [1, 10000]之间。
+// nums 的每个元素都将在 [-9999, 9999]之间。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/binary-search
+function search(nums:number[],target:number) {
+  // 全部遍历
+  let [left,right] = [0,nums.length - 1]
+  while (left <= right) {
+    if(nums[left] === target) return left 
+    else ++ left
+    if( nums[right] === target) return right
+    else --right
+  }
+  return -1
+}
+
+function search1(nums:number[],target:number) {
+  // 二分
+  let [left,right] = [0,nums.length - 1]
+  while (left <= right) {
+    const mid = Math.floor((right - left)/2) +left
+    if(nums[mid] === target){
+      return mid
+    }else if (nums[mid] > target){
+      right = mid -1
+    }else{
+      left = mid + 1
+    }
+  }
+  return -1
+}
+
+function testSearch() {
+  const results =  search1([5],5)
+  console.log(results)
+}
+
+testSearch()
