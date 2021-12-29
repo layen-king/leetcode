c5e851c67d1d445f7a8b7e7cd47963c4f7a9f835
layen <446495695@qq.com>
2021-12-29 14:37:58 +0800

    .

---
+++ typescript/countQuadruplets.ts
@@ -0,0 +1,72 @@
+// 统计特殊四元组
+
+// 简单
+
+// 给你一个 下标从 0 开始 的整数数组 nums ，返回满足下述条件的 不同 四元组 (a, b, c, d) 的 数目 ：
+
+// nums[a] + nums[b] + nums[c] == nums[d] ，且
+// a < b < c < d
+//  
+
+// 示例 1：
+
+// 输入：nums = [1,2,3,6]
+// 输出：1
+// 解释：满足要求的唯一一个四元组是 (0, 1, 2, 3) 因为 1 + 2 + 3 == 6 。
+// 示例 2：
+
+// 输入：nums = [3,3,6,4,5]
+// 输出：0
+// 解释：[3,3,6,4,5] 中不存在满足要求的四元组。
+// 示例 3：
+
+// 输入：nums = [1,1,1,3,5]
+// 输出：4
+// 解释：满足要求的 4 个四元组如下：
+// - (0, 1, 2, 3): 1 + 1 + 1 == 3
+// - (0, 1, 3, 4): 1 + 1 + 3 == 5
+// - (0, 2, 3, 4): 1 + 1 + 3 == 5
+// - (1, 2, 3, 4): 1 + 1 + 3 == 5
+//  
+
+// 提示：
+
+// 4 <= nums.length <= 50
+// 1 <= nums[i] <= 100
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/count-special-quadruplets
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+function countQuadruplets(nums: number[]): number {
+    let result = 0
+    for (let i = 0; i < nums.length - 3; i++) {
+      for (let j = i + 1; j < nums.length - 2; j++) {
+        for (let k = j + 1; k < nums.length - 1; k++) {
+          if ( nums.some(n => n === nums[i] + nums[j] + nums[k])) result++
+        }
+      }
+    }
+    return result
+}
+
+const countQuadruplets1 = (nums:number[]) => {
+  const len = nums.length;
+  let res = 0;
+  const map:Record<string, number>= {};
+  for (let c = len - 2; c >= 2; c--) {
+      map[nums[c + 1]] = (map[nums[c + 1]] || 0) + 1;
+      for (let a = 0; a < c; a++) {
+          for (let b = a + 1; b < c; b++) {
+              res += map[nums[a] + nums[b] + nums[c]] || 0;
+          }
+      }
+  }
+  return res;
+};
+
+function testCountQuadruplets(){
+  const result = countQuadruplets([2,16,9,27,3,39])
+  console.log(result)
+}
+
+testCountQuadruplets()
